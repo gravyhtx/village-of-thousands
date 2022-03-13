4755969f254187e7c29dc3f25be3f68293ffc7a5
Andres Long <andreslong95@gmail.com>
2022-03-13 18:02:57 -0500

    started cart

---
+++ utils/helpers.js
@@ -0,0 +1,47 @@
+export function idbPromise(storeName, method, object) {
+    return new Promise((resolve, reject) => {
+        const request =  window.indexedDB.open('vot-shop', 1);
+        let db, tx, store;
+        request.onupgradeneeded = function(e) {
+            const db = request.result;
+            db.createObjectStore('cart', { keyPath: 'product' });
+        };
+
+        request.onerror = function(e) { 
+            console.log('There was an error');
+        };
+
+        request.onsuccess = function(e) {
+            db = request.result;
+            tx = db.transaction(storeName, 'readwrite');
+            store = tx.objectStore(storeName);
+
+            db.onerror = function(e) {
+                console.log('error', e);
+            };
+
+            switch (method) {
+                case 'put':
+                    store.put(object);
+                    resolve(object);
+                    break;
+                case 'get':
+                    const all = store.getAll();
+                    all.onsuccess = function() {
+                        resolve(all.result);
+                    };
+                    break;
+                case 'delete':
+                    store.delete(object.product);
+                    break;
+                default:
+                    console.log('Not a valid method');
+                    break;
+            }
+
+            tx.oncomplete = function() {
+                db.close();
+            };
+        };
+    });
+}
