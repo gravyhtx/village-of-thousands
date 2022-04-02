47d534878c67a6c5d40d6f6847bb3f98afa206a5
Gravy Design Co <godisgravy@gmail.com>
2022-04-02 15:23:33 -0500

    Changed dbConnect db variable back to original

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -7,8 +7,7 @@ async function dbConnect() {
     return
   }
 
-  // const db = await mongoose.connect(process.env.MONGODB_URI)
-  const db = await mongoose.connect('mongodb://localhost:27017/votTestEnvi')
+  const db = await mongoose.connect(process.env.MONGODB_URI)
 
   connection.isConnected = db.connections[0].readyState;
 }
