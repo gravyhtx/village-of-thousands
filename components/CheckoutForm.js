e17200aabc501196f2f9c416018ea58709dde9f2
Andres Long <andreslong95@gmail.com>
2022-08-01 10:33:48 -0500

    checking white screen

--- components/CheckoutForm.js
+++ components/CheckoutForm.js
@@ -77,50 +77,50 @@ const CheckoutForm = ({ paymentIntent }) => {
         event.preventDefault();
 
         try {
-            const { error, paymentIntent: { status } } = await stripe.confirmCardPayment(paymentIntent.client_secret, {
-                payment_method: {
-                    card: elements.getElement(CardElement)
-                }
-            })
+            // const { error, paymentIntent: { status } } = await stripe.confirmCardPayment(paymentIntent.client_secret, {
+            //     payment_method: {
+            //         card: elements.getElement(CardElement)
+            //     }
+            // })
 
-            if (error) throw new Error(error.message);
+            // if (error) throw new Error(error.message);
 
-            if (status === 'succeeded') {
-                destroyCookie(null, "paymentIntentId")
-                setCheckoutSuccess(true);
+            // if (status === 'succeeded') {
+            //     destroyCookie(null, "paymentIntentId")
+            //     setCheckoutSuccess(true);
 
-                const user = await Auth.getProfile();
+            //     const user = await Auth.getProfile();
 
-                const productsOrdered = [];
-                cart.forEach(item => {
-                    productsOrdered.push(item.id)
-                })
+            //     const productsOrdered = [];
+            //     cart.forEach(item => {
+            //         productsOrdered.push(item.id)
+            //     })
 
-                const orderObj = {
-                    id: user.data._id,
-                    products: productsOrdered,
-                    addressCheck,
-                    shippingAddress: {
-                        ...userFormData
-                    },
-                    billingAddress: {
-                        ...userFormData
-                    },
-                    paymentConfirmation: orderId,
-                    totalPrice: totalAmount(cart),
-                    specialInstructions: "None"
-                }
-                await createOrder(orderObj)
+            //     const orderObj = {
+            //         id: user.data._id,
+            //         products: productsOrdered,
+            //         addressCheck,
+            //         shippingAddress: {
+            //             ...userFormData
+            //         },
+            //         billingAddress: {
+            //             ...userFormData
+            //         },
+            //         paymentConfirmation: orderId,
+            //         totalPrice: totalAmount(cart),
+            //         specialInstructions: "None"
+            //     }
+            //     await createOrder(orderObj)
 
-                const cartDeleteObj = {
-                    id: user.data._id
-                }
-                await idbPromise("delete", cartDeleteObj)
+            //     const cartDeleteObj = {
+            //         id: user.data._id
+            //     }
+            //     await idbPromise("delete", cartDeleteObj)
 
                 setTimeout(() => {
                     useRouter('/checkout/confirmation')
                 }, 3000)
-            };
+            // };
 
             setUserFormData({
                 first_name: "",
@@ -201,14 +201,15 @@ const CheckoutForm = ({ paymentIntent }) => {
                 </div> : <></>}
             <div className='row center checkout-details_submit'>
                 <div className='col s12'>
-                    {updateSuccess ? 
+                    <button className='theme-btn pay-button' type='submit'>SUBMIT</button>
+                    {/* {updateSuccess ? 
                     (
                         <button className='theme-btn pay-button' type='submit' disabled={!stripe}>SUBMIT</button>
                     )
                     :
                     (
                         <button className='theme-btn pay-button' type='submit' disabled={true}>SUBMIT</button>
-                    )}
+                    )} */}
                 </div>
             </div>
 
