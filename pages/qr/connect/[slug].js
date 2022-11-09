3e3f2cc7d25ae07abbb04a8bd55326672d91679f
Andres Long <andreslong95@gmail.com>
2022-11-09 16:51:23 -0600

    removed empty QR pages

--- pages/qr/connect/[slug].js
+++
@@ -1,6 +0,0 @@
-// Custom VoT "Link Tree " page for each team member
-// Customize our pages as personalized business card
-// Need to get head shot of each member to create single color vector
-//  - Create shadowed features for Avatar of each of out facees, inverted as white shadows for our faces
-//  - Bios (position/title, a bit about us, quote, etc
-//  - Links to contact
